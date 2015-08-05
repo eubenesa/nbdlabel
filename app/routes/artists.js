33d0de0e9d6ecefe0f464bbc2aa987a42cf00b91
Eubene Sa <eubene.sa@gmail.com>
2015-08-05 10:59:31 -0700

    Bundle update, update elevator.js and Font Awesome to v4.4.0, copy edit for work page

--- app/routes/artists.js
+++ app/routes/artists.js
@@ -21,6 +21,16 @@ export default Ember.Route.extend({
         scUrl: 'https://soundcloud.com/avidwalker',
         instaUrl: 'https://instagram.com/avid_walker',
         fbUrl: 'https://www.facebook.com/avidwalkermusic',
+        twUrl: 'https://twitter.com/Avid_Walker'
+      },
+      {
+        id: 3,
+        name: 'plazas',
+        imgUrl: 'assets/images/plazas.jpg',
+        blurb: 'Savana Salloum uses the name PLAZAS, as it depicts an idea of space and time, which relates directly to the sonic landscapes created in her songs. PLAZAS experiments with genres, having a wide array of influences ranging from electronica to pop, new wave, and ambient music.',
+        scUrl: 'https://soundcloud.com/plazasmusic',
+        instaUrl: 'https://instagram.com/plazas.wav',
+        fbUrl: 'https://www.facebook.com/plazasofficial',
         twUrl: ''
       }
     ];
