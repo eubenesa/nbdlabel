e65d3cc3572207f8b92e2eabe958581e8d9064dd
Eubene Sa <eubene.sa@gmail.com>
2015-10-22 09:17:37 -0700

    Add Bandcamp link for Mesa Luna, remove ember-cli-divshot, npm & bower updates

--- app/pods/artists/route.js
+++ app/pods/artists/route.js
@@ -11,6 +11,7 @@ export default Ember.Route.extend({
         photoByUrl: 'http://amandaanderson.xyz/',
         blurb: 'With their textural beats and hazy soundscapes alongside emotive glittering melodies, Justice McLellan and Alex Cooper create both catharsis and escapism. Their intricate musical explorations are the result of a transformative marriage of contradictory worlds; from opposing ends of the musical spectrum, Mesa Luna is born as a kaleidoscope of experimental pop sounds.',
         scUrl: 'https://soundcloud.com/mesaluna',
+        bcUrl: 'http://mesaluna.bandcamp.com/',
         instaUrl: 'https://instagram.com/mesa_luna',
         fbUrl: 'https://www.facebook.com/mesaluna',
         twUrl: 'https://twitter.com/mesa_luna_'
@@ -23,6 +24,7 @@ export default Ember.Route.extend({
         photoByUrl: 'http://amandaanderson.xyz/',
         blurb: 'Brendan Bleker and Jared Langerud. A couple pals making sun-soaked, melodic lo-fi pop in a bedroom studio. Playful but sincere, vulnerable yet confident, there is an air of melancholy woven into Avid Walker\'s songs that infuses their lightness and naivety with an emotional depth.',
         scUrl: 'https://soundcloud.com/avidwalker',
+        bcUrl: '',
         instaUrl: 'https://instagram.com/avid_walker',
         fbUrl: 'https://www.facebook.com/avidwalkermusic',
         twUrl: 'https://twitter.com/Avid_Walker'
@@ -35,6 +37,7 @@ export default Ember.Route.extend({
         photoByUrl: 'http://laurenjray.com/',
         blurb: 'Savana Salloum uses the name PLAZAS, as it depicts an idea of space and time, which relates directly to the sonic landscapes created in her songs. PLAZAS experiments with genres, having a wide array of influences ranging from electronica to pop, new wave, and ambient music.',
         scUrl: 'https://soundcloud.com/plazasmusic',
+        bcUrl: '',
         instaUrl: 'https://instagram.com/plazas.wav',
         fbUrl: 'https://www.facebook.com/plazasofficial',
         twUrl: ''
