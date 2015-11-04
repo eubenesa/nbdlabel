e44fa8deb60ad7d6fa81d8ac31fa925fbc10948e
Eubene Sa <eubene.sa@gmail.com>
2015-11-03 23:57:17 -0800

    Add computed property for releases, cleanup

--- app/mirage/fixtures/artists.js
+++ app/mirage/fixtures/artists.js
@@ -1,9 +1,9 @@
 export default [
   {
     id: 1,
-    name: 'mesa luna',
+    name: 'Mesa Luna',
     photo: 'assets/images/mesa-luna.jpg',
-    'photo-by': 'amanda anderson',
+    'photo-by': 'Amanda Anderson',
     'photo-by-link': 'http://amandaanderson.xyz/',
     blurb: 'With their textural beats and hazy soundscapes alongside emotive glittering melodies, Justice McLellan and Alex Cooper create both catharsis and escapism. Their intricate musical explorations are the result of a transformative marriage of contradictory worlds; from opposing ends of the musical spectrum, Mesa Luna is born as a kaleidoscope of experimental pop sounds.',
     soundcloud: 'https://soundcloud.com/mesaluna',
@@ -14,28 +14,25 @@ export default [
   },
   {
     id: 2,
-    name: 'avid walker',
+    name: 'Avid Walker',
     photo: 'assets/images/avid-walker.jpg',
-    'photo-by': 'amanda anderson',
+    'photo-by': 'Amanda Anderson',
     'photo-by-link': 'http://amandaanderson.xyz/',
     blurb: 'Brendan Bleker and Jared Langerud. A couple pals making sun-soaked, melodic lo-fi pop in a bedroom studio. Playful but sincere, vulnerable yet confident, there is an air of melancholy woven into Avid Walker\'s songs that infuses their lightness and naivety with an emotional depth.',
     soundcloud: 'https://soundcloud.com/avidwalker',
-    bandcamp: '',
     instagram: 'https://instagram.com/avid_walker',
     facebook: 'https://www.facebook.com/avidwalkermusic',
     twitter: 'https://twitter.com/Avid_Walker'
   },
   {
     id: 3,
-    name: 'plazas',
+    name: 'PLAZAS',
     photo: 'assets/images/plazas.jpg',
-    'photo-by': 'lauren ray',
+    'photo-by': 'Lauren Ray',
     'photo-by-link': 'http://laurenjray.com/',
     blurb: 'Savana Salloum uses the name PLAZAS, as it depicts an idea of space and time, which relates directly to the sonic landscapes created in her songs. PLAZAS experiments with genres, having a wide array of influences ranging from electronica to pop, new wave, and ambient music.',
     soundcloud: 'https://soundcloud.com/plazasmusic',
-    bandcamp: '',
     instagram: 'https://instagram.com/plazas.wav',
-    facebook: 'https://www.facebook.com/plazasofficial',
-    twitter: ''
+    facebook: 'https://www.facebook.com/plazasofficial'
   }
 ];
--- app/pods/release/model.js
+++ app/pods/release/model.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   cover: DS.attr('string'),
@@ -9,5 +10,15 @@ export default DS.Model.extend({
   blurb: DS.attr('string'),
   bandcamp: DS.attr('string'),
   itunes: DS.attr('string'),
-  spotify: DS.attr('string')
+  spotify: DS.attr('string'),
+
+  nbdId: Ember.computed('id', function() {
+    let id = this.get('id');
+    if (id < 10) {
+      id = 'nbd00' + id;
+    } else if (id < 100) {
+      id = 'nbd0' + id;
+    }
+    return id;
+  })
 });
