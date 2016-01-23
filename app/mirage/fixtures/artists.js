d0ab197323d56b1b0c15be6067ea26433c689576
Eubene Sa <eubene.sa@gmail.com>
2016-01-23 15:10:11 -0800

    Add Casual Luxury

--- app/mirage/fixtures/artists.js
+++ app/mirage/fixtures/artists.js
@@ -8,7 +8,7 @@ export default [
     blurb: 'With their textural beats and hazy soundscapes alongside emotive glittering melodies, Justice McLellan and Alex Cooper create both catharsis and escapism. Their intricate musical explorations are the result of a transformative marriage of contradictory worlds; from opposing ends of the musical spectrum, Mesa Luna is born as a kaleidoscope of experimental pop sounds.',
     soundcloud: 'https://soundcloud.com/mesaluna',
     bandcamp: 'http://mesaluna.bandcamp.com/',
-    instagram: 'https://instagram.com/mesa_luna',
+    instagram: 'https://www.instagram.com/mesa_luna/',
     facebook: 'https://www.facebook.com/mesaluna',
     twitter: 'https://twitter.com/mesa_luna_'
   },
@@ -20,7 +20,7 @@ export default [
     'photo-by': 'Amanda Anderson',
     blurb: 'Brendan Bleker and Jared Langerud. A couple pals making sun-soaked, melodic lo-fi pop in a bedroom studio. Playful but sincere, vulnerable yet confident, there is an air of melancholy woven into Avid Walker\'s songs that infuses their lightness and naivety with an emotional depth.',
     soundcloud: 'https://soundcloud.com/avidwalker',
-    instagram: 'https://instagram.com/avid_walker',
+    instagram: 'https://www.instagram.com/avid_walker/',
     facebook: 'https://www.facebook.com/avidwalkermusic',
     twitter: 'https://twitter.com/Avid_Walker'
   },
@@ -32,7 +32,19 @@ export default [
     'photo-by': 'Lauren Ray',
     blurb: 'Savana Salloum uses the name PLAZAS, as it depicts an idea of space and time, which relates directly to the sonic landscapes created in her songs. PLAZAS experiments with genres, having a wide array of influences ranging from electronica to pop, new wave, and ambient music.',
     soundcloud: 'https://soundcloud.com/plazasmusic',
-    instagram: 'https://instagram.com/plazas.wav',
+    instagram: 'https://www.instagram.com/plazas.wav/',
     facebook: 'https://www.facebook.com/plazasofficial'
+  },
+  {
+    id: 4,
+    name: 'Casual Luxury',
+    slug: 'casual-luxury',
+    photo: 'assets/images/casual-luxury.jpg',
+    'photo-by': 'Laura Genschorek',
+    blurb: 'Using familiar sounds with diverse structure, multi-instrumentalist Ethan Hall sheds light on the subconscious build up of emotion. With aggressive yet subtle feeling, Ethan draws on simplicity and honesty. A fever dream of guitars, drums and distant vocals, Casual Luxury uses these textures as a palette to create sound and song.',
+    soundcloud: 'https://soundcloud.com/casual-luxury',
+    bandcamp: 'http://casualluxuryband.bandcamp.com/',
+    instagram: 'https://www.instagram.com/casualluxury_band/',
+    facebook: 'https://www.facebook.com/casualluxuryband',
   }
 ];
